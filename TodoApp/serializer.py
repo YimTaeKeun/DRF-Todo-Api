5bbeedec237310ea98cf9c295cd449dcb1159e48
YimTaekeun <ytk030305@gmail.com>
2024-09-16 18:53:35 +0900

    [FIX] 시리얼라이저 이름 수정

--- TodoApp/serializer.py
+++ TodoApp/serializer.py
@@ -8,7 +8,7 @@ class TodoListSerializer(serializers.ModelSerializer):
         fields = ('title', 'important', 'complete')
 
 # todo를 만들거나, 상세정보를 가져올 때 사용하는 시리얼라이저 입니다.
-class TodoCreateSerializer(serializers.ModelSerializer):
+class TodoDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('title', 'description', 'important', 'complete')
